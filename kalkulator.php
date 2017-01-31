d12938ef37ec883cce0a946dcbc9395f8ac7d03d
danbraj <danbraj@users.noreply.github.com>
2017-01-31 21:30:28 +0100

    refaktoryzacja kodu 2

--- kalkulator.php
+++ kalkulator.php
@@ -3,86 +3,20 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 use KKP\KKPApi;
-use KKP\DataBase;
 
-if (KKPApi::isConsole()) {
-    if (count($argv) > 2) {
-        if ($argv[1] == '-rekord' && is_numeric($argv[2])) {
+KKPApi::isConsole(isset($argv) ? $argv : null);
 
-            $oDataBase = new DataBase();
-            $oDataBase->createTableIfNotExists();
-
-            if ($row = $oDataBase->getRowById($argv[2])) KKPApi::showCalculationResult($row);
-            else echo 'Nie ma zapisanego rekordu o takim id.';
-
-            exit();
-        }
-        else if (is_numeric($argv[1]) && is_numeric($argv[2]) && $argv[2] >= 0 && $argv[2] < 3) {
-
-            $tablica['kwota_netto'] = $argv[1];
-            $tablica['typ_umowy'] = $argv[2];
-            $czyZapis = false;
-            if (isset($argv[3]) && filter_var($argv[3], FILTER_VALIDATE_BOOLEAN)) $czyZapis = $argv[3];
-            
-            $result = KKPApi::calculateCosts($tablica['typ_umowy'], $tablica['kwota_netto']);
-            $row = array_merge($tablica, $result);
-            KKPApi::showCalculationResult($row);
-            
-            if ($czyZapis) {
-                $oDataBase = new DataBase();
-                $oDataBase->createTableIfNotExists();
-
-                $idLast = $oDataBase->addRowToDataBaseAndGetId($row);
-                echo "\nMozesz odtworzyc ten wynik poprzez wpisujac: php {$argv[0]} -rekord {$idLast}";
-            }
-            exit();
-        }
-        else echo "Niepoprawna skladnia.\n";
-    }
-    else {
-        KKPApi::showProgramDescription($argv[0]);
-        exit();
-    }  
-}
-else {
-    if (isset($_GET['q']) && is_numeric($_GET['q'])) {  
-        
-        $oDataBase = new DataBase();
-        $oDataBase->createTableIfNotExists();
-
-        if ($row = $oDataBase->getRowById($_GET['q'])) KKPApi::showCalculationResult($row);
-        else echo 'Nie ma zapisanego rekordu o takim id.';
-
-        exit();
-    }
-    else {
-        if (isset($_POST['kwota_netto']) && isset($_POST['typ_umowy'])) {
-            if (is_numeric($_POST['kwota_netto']) && is_numeric($_POST['typ_umowy']) && $_POST['typ_umowy'] >=0 && $_POST['typ_umowy'] < 3) {
-
-                $tablica['typ_umowy'] = $_POST['typ_umowy'];
-                $tablica['kwota_netto'] = $_POST['kwota_netto'];
-                if (isset($_POST['zapis'])) $czyZapis = true;
-                else $czyZapis = false;
-
-                $result = KKPApi::calculateCosts($tablica['typ_umowy'], $tablica['kwota_netto']);
-                $row = array_merge($tablica, $result);
-                KKPApi::showCalculationResult($row);
-
-                if ($czyZapis) {
-                    $oDataBase = new DataBase();
-                    $oDataBase->createTableIfNotExists();
-
-                    $idLast = $oDataBase->addRowToDataBaseAndGetId($row);
-                    $sLink = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'].'?q='.$idLast;
-                    echo "Mozesz odtworzyc ten wynik używając linka: <a href='{$sLink}'>{$sLink}</a>";
-                }
-                exit();
-            }
-            else echo 'Proszę nie kombinować!';
-        }
-        else {
-            require "formularz.html";
-            exit();
-        }
-    }
+switch(KKPApi::getEvent()) {
+    case 1:
+        KKPApi::doCalculations();
+        break;
+    case 2:
+        KKPApi::showSavedRecord();
+        break;
+    case -1:
+        echo "Niepoprawna skladnia.";
+        break;
+    default:
+        if (KKPApi::$isConsole) KKPApi::showProgramDescription();
+        else require "formularz.html";
 }
--- src/KKP/KKPApi.php
+++ src/KKP/KKPApi.php
@@ -5,28 +5,30 @@ namespace KKP;
 class KKPApi {
 
     static $aUmowy = array("Umowa zlecenie", "Umowa o dzielo", "Umowa o prace");
-    static $isConsole;
-    static $pattern;
+    static $isConsole = false;
+    static $aArgs = array();
+    static $pattern = null;
 
-    static function showProgramDescription($sFileName) {
-        echo "\n\tSkladnia:\tphp {$sFileName} [kwota_netto] [typ_umowy] [zapis = false]\n";
-        echo "\t\t\tphp {$sFileName} -rekord [id]\n\n";
+    static function showProgramDescription() {
+        echo "\n\tSkladnia:\tphp ".self::$aArgs[0]." [kwota_netto] [typ_umowy] [zapis = false]\n";
+        echo "\t\t\tphp ".self::$aArgs[0]." -rekord [id]\n\n";
         echo "\tid\t\tliczba\n";
         echo "\tkwota_netto\tliczba\n";
-        echo "\ttyp_umowy\t0 => Umowa zlecenie\n";
-        echo "\t\t\t1 => Umowa o dzielo\n";
-        echo "\t\t\t2 => Umowa o prace\n";
+        echo "\ttyp_umowy\t0 => ".self::$aUmowy[0]."\n";
+        echo "\t\t\t1 => ".self::$aUmowy[1]."\n";
+        echo "\t\t\t2 => ".self::$aUmowy[2]."\n";
         echo "\tzapis\t\twartosc logiczna (true, 1, false, 0)\n";
     }
 
-    static function isConsole() {
+    static function isConsole($argv = null) {
         if (PHP_SAPI === 'cli') {
-            self::$pattern = "Rodzaj umowy: %s\nWartosc netto: %s\nWartosc brutto: %s\nKosz pracodawcy: %s";
+            self::$pattern = "Rodzaj umowy:\t\t%s\nWartosc netto:\t\t%s zl\nWartosc brutto:\t\t%s zl\nKosz pracodawcy:\t%s zl";
             self::$isConsole = true;
+            self::$aArgs = $argv;
             return true;
         }
         else {
-            self::$pattern = "<p>Rodzaj umowy: %s</p><p>Wartosc netto: %s</p><p>Wartosc brutto: %s</p><p>Kosz pracodawcy: %s</p>";
+            self::$pattern = "<table><tr><td>Rodzaj umowy:</td><td>%s</td></tr><tr><td>Wartosc netto:</td><td>%s zł</td></tr><tr><td>Wartosc brutto:</td><td>%s zł</td></tr><tr><td>Kosz pracodawcy:</td><td>%s zł</td></tr></table>";
             self::$isConsole = false;
             return false;
         }
@@ -60,5 +62,62 @@ class KKPApi {
                 $result['koszt_pracodawcy'] = 2;
         }
         return $result;
+    }
+
+    static function getEvent() {
+        if (self::$isConsole)
+            if (count(self::$aArgs) > 2)
+                if (self::$aArgs[1] == '-rekord' && is_numeric(self::$aArgs[2])) return 2; // con rekord
+                else if (is_numeric(self::$aArgs[1]) && is_numeric(self::$aArgs[2]) && self::$aArgs[2] >= 0 && self::$aArgs[2] < 3)
+                    return 1; // con - poprawna skladnia
+                else return -1; // con - nie poprawna skladnia
+            else return 0; // con - opis dzialania
+        else
+            if (isset($_GET['q']) && is_numeric($_GET['q'])) return 2; // www rekord
+            else
+                if (isset($_POST['kwota_netto']) && isset($_POST['typ_umowy']))
+                    if (is_numeric($_POST['kwota_netto']) && is_numeric($_POST['typ_umowy']) && $_POST['typ_umowy'] >=0 && $_POST['typ_umowy'] < 3)
+                        return 1; // www - poprawna skladnia
+                    else return -1; // www - niepoprawna skladnia
+                else return 0; // www - formularz
+    }
+
+    static function doCalculations() {
+        if (self::$isConsole) {
+            $tablica['kwota_netto'] = self::$aArgs[1];
+            $tablica['typ_umowy'] = self::$aArgs[2];
+            $czyZapis = false;
+            if (isset(self::$aArgs[3]) && filter_var(self::$aArgs[3], FILTER_VALIDATE_BOOLEAN)) $czyZapis = self::$aArgs[3];
+        }
+        else {
+            $tablica['typ_umowy'] = $_POST['typ_umowy'];
+            $tablica['kwota_netto'] = $_POST['kwota_netto'];
+            if (isset($_POST['zapis'])) $czyZapis = true;
+            else $czyZapis = false;
+        }
+
+        $result = self::calculateCosts($tablica['typ_umowy'], $tablica['kwota_netto']);
+        $row = array_merge($tablica, $result);
+        self::showCalculationResult($row);
+
+        if ($czyZapis) {
+            $oDataBase = new DataBase();
+            $oDataBase->createTableIfNotExists();
+
+            $idLast = $oDataBase->addRowToDataBaseAndGetId($row);
+            if (self::$isConsole)
+                echo "\nWynik ten mozna ponownie zobaczyc wpisujac: php ".self::$aArgs[0]." -rekord {$idLast}";
+            else {
+                $sLink = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'].'?q='.$idLast;
+                echo "Wynik ten dostępny jest pod adresem: <a href='{$sLink}'>{$sLink}</a>";
+            }   
+        }
+    }
+
+    static function showSavedRecord() {
+        $oDataBase = new DataBase();
+        $oDataBase->createTableIfNotExists();
+        if ($row = $oDataBase->getRowById(self::$isConsole ? self::$aArgs[2] : $_GET['q'])) self::showCalculationResult($row);
+        else echo 'Nie ma zapisanego rekordu o takim id.';
     }
 }
